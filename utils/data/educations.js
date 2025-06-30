1fe772e871f46ed1f06bdbff32e4b5689bb80bbf
Lucmon113 <99042443+Lucmon113@users.noreply.github.com>
2025-06-30 22:11:59 +0200

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,14 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "Bachelor's Degree",
+    duration: "2025 - Present",
+    institution: "Gisma University of Applied Sciences, Berlin",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
-  },
-  {
-    id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    title: "Higher education Certificate",
+    duration: "2019 - 2023",
+    institution: "Reina del Mundo, Lima",
   }
 ]
